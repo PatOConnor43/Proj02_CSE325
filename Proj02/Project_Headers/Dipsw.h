cf620af421d5fbe3baeda788c577a0cdd8feeae6
Patrick O'Connor <patoconnor43@gmail.com>
2015-02-08 17:43:07 -0700

    Upload Dipsw header

---
+++ Proj02/Project_Headers/Dipsw.h
@@ -0,0 +1,15 @@
+/*
+ * Dipsw.h
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: paoconno
+ */
+
+#ifndef DIPSW_H_
+#define DIPSW_H_
+static int DipState;
+
+void initDip();
+int getDipState();
+
+#endif /* DIPSW_H_ */
